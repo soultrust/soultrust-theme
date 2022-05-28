f64b4546f3df06cd67ab0b55ff9d6ea61b12f16d
Tomo Suzuki <tomo@soultrust.com>
2022-05-28 05:14:16 -0400

    Rounded corners on site layout

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
   <?php wp_head() ?>
 </head>
 <body <?php body_class(); ?>>
-
+<div class="site">
 <?php if (is_front_page() && is_home()) { ?>
   <h1><?php echo get_bloginfo('name'); ?></h1>
 <?php } else { ?>
