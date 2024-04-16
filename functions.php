847bbd27e06765ac4f95a2dd4ba72cd19efe97f2
Tomo Suzuki <tomo@soultrust.com>
2024-04-16 17:17:04 -0400

    Add function to turn on categories/tags for pages

--- functions.php
+++ functions.php
@@ -173,7 +173,14 @@ function soultrust_customize_color_css() { ?>
 <?php }
 add_action('wp_head', 'soultrust_customize_color_css');
 
-
+function myplugin_settings() {  
+    // Add tag metabox to page
+    register_taxonomy_for_object_type('post_tag', 'page'); 
+    // Add category metabox to page
+    register_taxonomy_for_object_type('category', 'page');  
+}
+ // Add to the admin_init hook of your theme functions.php file 
+add_action( 'init', 'myplugin_settings' );
 // add_filter( 'enter_title_here', 'custom_enter_title_text' );
 
 // function custom_enter_title_text( $input ) {
